edbd8454f9693c80420481b229a281ec68c9274f
stuckyb <stuckyb@users.noreply.github.com>
2021-09-01 11:48:33 -0400

    Initial implementation of DatasetCatalog class.

---
+++ src/catalog/catalog.py
@@ -0,0 +1,49 @@
+
+from pathlib import Path
+import json
+
+
+class DatasetCatalog:
+    def __init__(self, store_path):
+        """
+        store_path(str, Path, or PathLike): The location of on-disk dataset
+        storage.
+        """
+        self.store_path = Path(store_path)
+        self.datasets = {}
+
+    def addDatasetsByClass(self, *dataset_classes):
+        """
+        dataset_classes: One or more concrete subclasses of GSDataSet.
+        """
+        for dataset_class in dataset_classes:
+            dataset = dataset_class(self.store_path)
+            self.addDataset(dataset)
+
+    def addDataset(self, dataset):
+        """
+        Adds a dataset to the catalog.
+
+        dataset (GSDataSet): A dataset instance to add to the catalog.
+        """
+        self.datasets[dataset.id] = dataset
+
+    def getDatasetsListingJSON(self):
+        dsl = []
+        for key in self.datasets:
+            dsl.append({'id': key, 'name': self.datasets[key].name})
+
+        # Sort by dataset name.
+        dsl.sort(key=lambda item: item['name'])
+
+        return json.dumps(dsl)
+
+    def getDataset(self, dataset_id):
+        if dataset_id not in self.datasets:
+            raise KeyError(f'Invalid dataset ID: "{dataset_id}"')
+        else:
+            return self.datasets[dataset_id]
+
+    def __getitem__(self, dataset_id):
+        return self.getDataset(dataset_id)
+
