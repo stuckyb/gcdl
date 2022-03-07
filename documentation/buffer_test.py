287cc240beca184ff142b1637ef959eb7d8bb1e4
Heather Savoy <heather.savoy@usda.gov>
2022-03-07 09:13:13 -0700

    Added target resolution variability to buffer test
    for #38

--- documentation/buffer_test.py
+++ documentation/buffer_test.py
@@ -58,9 +58,10 @@ user_geom0 = gpd.GeoSeries([sh_poly], crs='EPSG:' + str(user_crs))
 
 aggs = [0.25, 0.5, 1, 2, 4, 10]
 epsgs = [4326,4269,3857,5070,2163,32613]
+res = [0.15, 1, 3, 12]
 
 ## Make some pre-made resampled clipped regions of PRISM
-if True: 
+if False: 
     data0 = rioxarray.open_rasterio(fpath, masked=True)
     datac = data0.rio.clip(box_geom0)
     for e in epsgs:
@@ -73,10 +74,11 @@ if True:
                 resolution=tuple(r*i for r in data.rio.resolution())
             )
             data.rio.to_raster("../output/prism_test_input_" + str(e) + '_' + str(i) + ".tif")
-            plt.figure(figsize=(6,8))
-            data.plot(cmap='RdBu', vmin=0, vmax=180)
-            plt.savefig('../output/buff_test' + str(e) + '_' + str(i) + '.png')
-            plt.close()
+            if False:
+                plt.figure(figsize=(6,8))
+                data.plot(cmap='RdBu', vmin=0, vmax=180)
+                plt.savefig('../output/buff_test' + str(e) + '_' + str(i) + '.png')
+                plt.close()
 
 
 ## For the different aggregations, starting with no buffer:
@@ -91,80 +93,85 @@ if True:
     #each user trial
     for target_e in epsgs:  
 
-        target_res = 0
+        t_res = 0
+        # I know these aren't exactly the same but I don't think it matters 
+        # since I'm not comparing target_e against each other
         if target_e in [2163,32613,3857,5070]: #meters
-            target_res = 4000
+            t_res = 4000.0
         else: #degrees
-            targe_res = 0.05
-
-        target_data = None
+            t_res = 0.04167
 
         # Reproject user geometry to this trial crs
         user_geom = user_geom0.to_crs('EPSG:' + str(target_e))
 
-        # Create list for collecting result DataArrays
-        data_sum = []
+        # Experimenting with finer/coarser resolution
+        for tr in res:
+            target_res = t_res*tr
 
-        #diff dataset CRS
-        for ds_e in epsgs:
+            target_data = None
 
-            # Reproject user geometry to match dataset
-            prism_geom = user_geom.to_crs('EPSG:' + str(ds_e))
+            # Create list for collecting result DataArrays
+            data_sum = []
 
-            #diff dataset resolution
-            for ds_a in aggs:
+            #diff dataset CRS
+            for ds_e in epsgs:
 
-                # Read in dataset
-                ds_path = "../output/prism_test_input_" + str(ds_e) + '_' + str(ds_a) + ".tif"
-                ds = rioxarray.open_rasterio(ds_path, masked=True)
+                # Reproject user geometry to match dataset
+                prism_geom = user_geom.to_crs('EPSG:' + str(ds_e))
 
-                # Create buffer: one cell
-                buffer_geom = prism_geom.buffer(2*max(ds.rio.resolution())) #*max(aggs)/ds_a) 
+                #diff dataset resolution
+                for ds_a in aggs:
 
-                # Clip dataset to buffered user geometry in dataset crs 
-                datac = ds.rio.clip(
-                    buffer_geom,
-                    all_touched = True
-                )
+                    # Read in dataset
+                    ds_path = "../output/prism_test_input_" + str(ds_e) + '_' + str(ds_a) + ".tif"
+                    ds = rioxarray.open_rasterio(ds_path, masked=True)
 
-                if ds_e == epsgs[0] and ds_a == aggs[0]: # if first dataset
-                    # Reproject clipped dataset to target crs
-                    datar = datac.rio.reproject(
-                        dst_crs = 'EPSG:' + str(target_e),
-                        resolution = target_res
-                    )
-                    # Clip to user geometry in target crs
-                    data_final = datar.rio.clip(
-                        user_geom,
+                    # Create buffer: what size?
+                    this_ds = max(ds.rio.resolution())  # too small -> border differences
+                    coarsest_ds = max(ds.rio.resolution())*max(aggs)/ds_a # works! -> no differences
+                    coarsest_overall = max(coarsest_ds, target_res) #doesn't seem to be needed
+                    buffer_geom = prism_geom.buffer(coarsest_ds)
+
+                    # Clip dataset to buffered user geometry in dataset crs 
+                    datac = ds.rio.clip(
+                        buffer_geom,
                         all_touched = True
                     )
-                    target_data = data_final
-                else:
-                    data_final = datac.rio.reproject_match(
-                        target_data
-                    )
-                    #data_final = datac.rio.reproject(
-                    #     dst_crs = 'EPSG:' + str(target_e),
-                    #    resolution = target_res
-                    #)
-                    data_final = data_final.rio.clip(
-                        user_geom,
-                        all_touched = True
-                    )
-                    
-                
-        
-                # Sum not null cells 
-                data_sum.append(data_final.notnull().astype('int'))
+
+                    if ds_e == epsgs[0] and ds_a == aggs[0]: # if first dataset
+                        # Reproject clipped dataset to target crs
+                        datar = datac.rio.reproject(
+                            dst_crs = 'EPSG:' + str(target_e),
+                            resolution = target_res
+                        )
+                        # Clip to user geometry in target crs
+                        data_final = datar.rio.clip(
+                            user_geom,
+                            all_touched = True
+                        )
+                        target_data = data_final
+                    else:
+                        data_final = datac.rio.reproject_match(
+                            target_data
+                        )
+                        #data_final = datac.rio.reproject(
+                        #     dst_crs = 'EPSG:' + str(target_e),
+                        #    resolution = target_res
+                        #)
+                        data_final = data_final.rio.clip(
+                            user_geom,
+                            all_touched = True
+                        )
+            
+                    # Sum not null cells 
+                    data_sum.append(data_final.notnull().astype('int'))
                 
               
-        data_sum = xr.concat(data_sum, dim='ds').sum('ds')
-        plt.figure(figsize=(6,8))
-        data_sum.plot(cmap='RdBu', vmin=0, vmax=len(epsgs)*len(aggs))
-        #plt.xlim([-111, -101])
-        #plt.ylim([30, 38.5])
-        plt.savefig('../output/prism_sum_r' + str(target_e) + '.png')
-
+            data_sum = xr.concat(data_sum, dim='ds').sum('ds')
+            plt.figure(figsize=(6,8))
+            data_sum.plot(cmap='RdBu', vmin=0, vmax=len(epsgs)*len(aggs))
+            plt.savefig('../output/prism_sum_r' + str(target_e) + '_' + str(tr) + '.png')
+            plt.close()
 
 
 
