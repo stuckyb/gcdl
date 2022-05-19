ff2bf245b7276e67773eadd9917bd6bb7fa444a5
Heather Savoy <heather.savoy@usda.gov>
2022-05-19 09:44:33 -0600

    started test script for DataRequestOutput

---
+++ src/tests/test_data_request_output.py
@@ -0,0 +1,39 @@
+
+import unittest
+
+
+class TestDataRequestOutput(unittest.TestCase):
+
+    def setUp(self):
+        # Set up a basic DataRequestOutput object.
+        self.dro = DataRequestOutput()
+
+    def test_getSingleLayerOutputFileName(self):
+        pass
+
+    def test_writeCSV(self):
+        pass
+
+    def test_writeShapefile(self):
+        pass
+
+    def _assignCategories(self):
+        pass
+
+    def test_writeNetCDF(self):
+        pass
+
+    def test_writeGeoTIFF(self):
+        pass
+
+    def test_writePointFiles(self):
+        pass
+
+    def test_writeRasterFiles(self):
+        pass
+
+    def test_writeMetadataFile(self):
+        pass
+
+     def test_writeRequestedData(self):
+        pass
