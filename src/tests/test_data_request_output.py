e977672c6a6288e65f82b91efacb28ceee238a80
Heather Savoy <heather.savoy@usda.gov>
2022-05-19 13:14:34 -0600

    did simple output tests

--- src/tests/test_data_request_output.py
+++ src/tests/test_data_request_output.py
@@ -1,5 +1,6 @@
 
 import unittest
+from api_core import data_request, DataRequestOutput
 
 
 class TestDataRequestOutput(unittest.TestCase):
@@ -9,7 +10,35 @@ class TestDataRequestOutput(unittest.TestCase):
         self.dro = DataRequestOutput()
 
     def test_getSingleLayerOutputFileName(self):
-        pass
+        dro = self.dro
+
+        # Test with date
+        exp = 'ds1_var1_2021'
+        r = dro._getSingleLayerOutputFileName('ds1','var1','2021')
+        self.assertEqual(exp, r)
+
+        # Test without date
+        exp = 'ds1_var1'
+        r = dro._getSingleLayerOutputFileName('ds1','var1','')
+        self.assertEqual(exp, r)
+
+    def test_rgbaToHex(self):
+        dro = self.dro
+
+        # Test white with alpha
+        exp = '#ffffff'
+        r = dro._rgbaToHex((255,255,255,255))
+        self.assertEqual(exp, r)
+
+        # Test white without alpha
+        exp = '#ffffff'
+        r = dro._rgbaToHex((255,255,255))
+        self.assertEqual(exp, r)
+
+        # Test some random color
+        exp = '#32a852'
+        r = dro._rgbaToHex((50, 168, 82))
+        self.assertEqual(exp, r)
 
     def test_writeCSV(self):
         pass
@@ -35,5 +64,5 @@ class TestDataRequestOutput(unittest.TestCase):
     def test_writeMetadataFile(self):
         pass
 
-     def test_writeRequestedData(self):
+    def test_writeRequestedData(self):
         pass
