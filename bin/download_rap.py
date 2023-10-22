2664e20b4d5bae90edfa8edf1127dc13325d0fe2
BrianStucky-USDA <BrianStucky-USDA@users.noreply.github.com>
2023-10-22 13:41:19 -0500

    Added utility script for downloading RAP data.

---
+++ bin/download_rap.py
@@ -0,0 +1,31 @@
+
+# Downloads RAP data files for annual vegetation cover and vegetation biomass.
+# Downloads multiple files concurrently to maximize transfer speed. Files that
+# already exist locally will not be downloaded again.
+
+from concurrent.futures import ThreadPoolExecutor
+import subprocess as sp
+import sys
+
+
+vegcover_baseurl = 'http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-cover/v3/vegetation-cover-v3-{0}.tif'
+vegcover_years = range(1986, 2023)
+biomass_baseurl = 'http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-biomass/v3/vegetation-biomass-v3-{0}.tif'
+biomass_years = range(1986, 2023)
+
+# Download up to 10 files concurrently.
+max_threads = 10
+
+def download_file(url):
+    print(f'Downloading {url}...')
+    sp.run(['wget', '-q', url])
+
+with ThreadPoolExecutor(max_workers=max_threads) as runner:
+    for year in vegcover_years:
+        url = vegcover_baseurl.format(year)
+        runner.submit(download_file, url)
+
+    for year in biomass_years:
+        url = biomass_baseurl.format(year)
+        runner.submit(download_file, url)
+
