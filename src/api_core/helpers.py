c2f90a037514aa6891a4be7bf932fc7c409a281d
NoaMillsUSDA-ARS <noa.mills@usda.gov>
2024-01-24 15:07:20 -0800

    Fixed crs/resolution issue that caused no data in bounds errors

--- src/api_core/helpers.py
+++ src/api_core/helpers.py
@@ -155,10 +155,8 @@ def get_target_crs(input_crs_str, resolution, user_geom):
     # in the clip geometry.
     if input_crs_str is not None:
         target_crs = CRS(input_crs_str)
-    elif input_crs_str is None and resolution is not None:
-        target_crs = user_geom.geom.crs
     else:
-        target_crs = None
+        target_crs = user_geom.geom.crs
 
     return target_crs
 
