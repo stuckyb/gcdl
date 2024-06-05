66429fba3ae29ebd26833e003e39a0db9f6c03ba
Noa Mills <noa.mills@usda.gov>
2024-06-05 10:46:29 -0700

    Fixed multiline string

--- src/api_core/upload_cache.py
+++ src/api_core/upload_cache.py
@@ -65,10 +65,10 @@ class DataUploadCache:
             fpath.unlink()
             max_mb = self.maxsize / (1024 * 1024)
             raise Exception(
-                'Uploaded file size exceeded maximum file size of {} MB.
+                '''Uploaded file size exceeded maximum file size of {} MB.
                 Please submit a GeoCDL github issue at 
                 github.com/stuckyb/gcdl/issues to request a larger 
-                file size limit, or decrease your file size.'.format(max_mb)
+                file size limit, or decrease your file size.'''.format(max_mb)
             )
 
         return guid
